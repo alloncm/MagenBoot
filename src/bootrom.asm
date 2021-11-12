f4b35d48fd32981e1d032c5b665886c5d051e185
Alon Cohen-Magen <alloncm@gmail.com>
2021-11-12 13:33:44 +0200

    Arrange the file and trying to draw to the screen
    Add memcpy function too
    Fix the memset function

--- src/bootrom.asm
+++ src/bootrom.asm
@@ -1,42 +1,81 @@
 SECTION "main", ROM0[$0]
 
-; init stack
-ld sp, $FFFE
+Main::
+    ld sp, $FFFE    ; init stack
+    di              ; interrupts are not needed in the boot
 
-; init vram
-ld hl, $8000    ; vram address
-ld bc, $2000    ; vram size
-xor a           ; zeroing a           
-call Memset
+    xor a           ; zeroing a   
 
-; init ram
-ld hl, $A000    ; ram address
-ld bc, $2000    ; ram size
-call Memset     ; a is alrady 0
+    ld hl, $8000    ; vram address
+    ld bc, $2000    ; vram size        
+    call .memset    ; init vram
 
-; init oam
-ld hl, $FE00    ; OAM adress
-ld c, $A0       ; OAM size, since 0xA0 is smaller than 0xFF no need to set the whole register
-call Memset
+    ld hl, $A000    ; ram address
+    ld bc, $2000    ; ram size
+    call .memset    ; init ram
+ 
+    ld hl, $FE00    ; OAM adress
+    ld bc, $00A0    ; OAM size
+    call .memset    ; init oam
 
-; turn on the lcd
-ld a, $80
-ld [$ff00+$40], a
+    ld hl, $9000    ; Tile data address
+    ld bc, .tile_start
+    ld de, .tile_end - .tile_start
+    call .memcpy
 
-jp FinishBoot
+    ; turn on the lcd
+    ld a, %10000001 ; Turn lcd and BG on
+    ld [$ff40], a
 
-; hl - address to set
-; bc - non zero length
-; a - value to set
-Memset:
-    ldi [hl], a
-    dec bc
-    jr nz, Memset
-    ret
+    .loop:
+        halt
+        jr .loop
 
-ds $100 - 4 - @, 0 ; filling the bootrom with 0
+    jp .finishBoot
 
-; writing to the bootrom register turning the boot off
-FinishBoot:
-    ld a, 1
-    ld [$ff00+$50], a
+    ; mut hl - dst (address to set)
+    ; mut bc - len (non zero length)
+    ; const a - val (value to set)
+    .memset:
+        ldi [hl], a
+        dec bc
+        ; detect if bc is zero and return if so
+        ld d, a     ; saves a 
+        xor a
+        cp a, c
+        jr nz, .continue
+        cp a, b
+        jr nz, .continue
+
+        ld a, d     ; loads a
+        ret
+        .continue:
+            ld a, d ; loads a
+            jr .memset
+    
+    ; mut hl - dst (address to copy to)
+    ; mut bc - src (adress to copy from)
+    ; mut de - len (length of src to copy to dst)
+    .memcpy:
+        ld a, [bc]
+        inc bc
+        ldi [hl], a
+        dec de
+        ; detect if de is zero
+        xor a
+        cp a, e
+        jr nz, .memcpy
+        cp a, d
+        jr nz, .memcpy
+        ret
+    
+    .tile_start
+        dw $FF, $FF, $FF, $FF, $FF, $FF, $FF, $FF
+    .tile_end
+
+    ds $100 - 4 - @, 0 ; filling the bootrom with 0
+
+    ; writing to the bootrom register turning the boot off
+    .finishBoot:
+        ld a, 1
+        ld [$ff50], a
