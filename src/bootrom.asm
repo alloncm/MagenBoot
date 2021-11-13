96a0c13ce924ee16d305f2ab0c29e0aa5432abf6
alloncm <alloncm@gmail.com>
2021-11-13 18:41:06 +0200

    Arrange the file

--- src/bootrom.asm
+++ src/bootrom.asm
@@ -8,26 +8,26 @@ Main::
 
     ld hl, $8000    ; vram address
     ld bc, $2000    ; vram size        
-    call .memset    ; init vram
+    call Memset    ; init vram
 
     ld hl, $C000    ; ram address
     ld bc, $2000    ; ram size
-    call .memset    ; init ram
+    call Memset    ; init ram
  
     ld hl, $FE00    ; OAM adress
     ld bc, $00A0    ; OAM size
-    call .memset    ; init oam
+    call Memset    ; init oam
 
     ld hl, $9903
     ld de, $9923
     ld b, 29
     ld a, 1
-    call .setTilemap
+    call SetTilemap
     
     ld hl, $8010
-    ld bc, .encodedTileDataStart
-    ld d, .encodedTileDataEnd - .encodedTileDataStart
-    call .decodeLogoToVram
+    ld bc, EncodedTileData.start
+    ld d, EncodedTileData.end - EncodedTileData.start
+    call DecodeLogoToVram
 
     ; turn on the lcd
     ld a, %11111100
@@ -39,112 +39,113 @@ Main::
         halt
         jr .loop
 
-    jp .finishBoot
+    jp FinishBoot
 
-    ; mut hl - start address of the tilemap
-    ; mut de - second start address
-    ; mut a - start index for the tiles
-    ; const b - max index of the tiles
-    .setTilemap
-        ldi [hl], a
-        inc a
-        cp a, b
-        jr z, .return
-        push hl
-        ld h, d
-        ld l, e
-        ldi [hl], a
-        inc a
-        ld d, h
-        ld e, l
-        pop hl
-        cp a, b
-        jr nz, .setTilemap
-        .return
-            ret
-
-    ; mut hl - dst (address to set)
-    ; mut bc - len (non zero length)
-    ; const a - val (value to set)
-    .memset:
-        ldi [hl], a
-        dec bc
-        ; detect if bc is zero and return if so
-        ld d, a     ; saves a 
-        xor a
-        cp a, c
-        jr nz, .continue
-        cp a, b
-        jr nz, .continue
-
-        ld a, d     ; loads a
+; mut hl - start address of the tilemap
+; mut de - second start address
+; mut a - start index for the tiles
+; const b - max index of the tiles
+SetTilemap:
+    ldi [hl], a
+    inc a
+    cp a, b
+    jr z, .return
+    push hl
+    ld h, d
+    ld l, e
+    ldi [hl], a
+    inc a
+    ld d, h
+    ld e, l
+    pop hl
+    cp a, b
+    jr nz, SetTilemap
+    .return
         ret
-        .continue:
-            ld a, d ; loads a
-            jr .memset
+
+; mut hl - dst (address to set)
+; mut bc - len (non zero length)
+; const a - val (value to set)
+Memset:
+    ldi [hl], a
+    dec bc
+    ; detect if bc is zero and return if so
+    ld d, a     ; saves a 
+    xor a
+    cp a, c
+    jr nz, .continue
+    cp a, b
+    jr nz, .continue
+
+    ld a, d     ; loads a
+    ret
+    .continue:
+        ld a, d ; loads a
+        jr Memset
+
+; mut hl - dst (address to copy to)
+; mut bc - src (adress to copy from)
+; mut de - len (length of src to copy to dst)
+Memcpy:
+    ld a, [bc]
+    inc bc
+    ldi [hl], a
+    dec de
+    ; detect if de is zero
+    xor a
+    cp a, e
+    jr nz, Memcpy
+    cp a, d
+    jr nz, Memcpy
+    ret
+
+; mut hl - pointer to vram
+; mut bc - pointer to encoded data
+; mut d - encoded data length
+DecodeLogoToVram:
+    push de
+    ld a, [bc]
+    push bc
+    ld d, a
+    ld e, a
+    ld c, 2
+    .decodeNibleToByte
+        ld b, 4
+        .mulBit
+            rl d
+            rla
+            rl e
+            rla
+            dec b
+            jr nz, .mulBit
+        ld b, 2
+        .loadValueToVram
+            ldi [hl], a
+            ld [hl], 0      ; redunant the vram is already zeroed
+            inc hl
+            dec b
+            jr nz, .loadValueToVram
+        dec c
+        jr nz, .decodeNibleToByte
+    pop bc
+    pop de
+    inc bc
+    dec d
+    jr nz, DecodeLogoToVram
+    ret
     
-    ; mut hl - dst (address to copy to)
-    ; mut bc - src (adress to copy from)
-    ; mut de - len (length of src to copy to dst)
-    .memcpy:
-        ld a, [bc]
-        inc bc
-        ldi [hl], a
-        dec de
-        ; detect if de is zero
-        xor a
-        cp a, e
-        jr nz, .memcpy
-        cp a, d
-        jr nz, .memcpy
-        ret
-    
-    ; mut hl - pointer to vram
-    ; mut bc - pointer to encoded data
-    ; mut d - encoded data length
-    .decodeLogoToVram:
-        push de
-        ld a, [bc]
-        push bc
-        ld d, a
-        ld e, a
-        ld c, 2
-        .decodeNibleToByte
-            ld b, 4
-            .mulBit
-                rl d
-                rla
-                rl e
-                rla
-                dec b
-                jr nz, .mulBit
-            ld b, 2
-            .loadValueToVram
-                ldi [hl], a
-                ld [hl], 0      ; redunant the vram is already zeroed
-                inc hl
-                dec b
-                jr nz, .loadValueToVram
-            dec c
-            jr nz, .decodeNibleToByte
-        pop bc
-        pop de
-        inc bc
-        dec d
-        jr nz, .decodeLogoToVram
-        ret
-        
 
-    .encodedTileDataStart:
+EncodedTileData:
+    .start:
         db $ce,$ff,$dd,$c0,$37,$ff,$bb,$30,$00,$70,$34,$30,$00,$8d,$dd,$c0
         db $00,$f9,$9f,$1f,$00,$9b,$bb,$90,$00,$e3,$f0,$f0,$00,$67,$66,$60
         db $00,$c6,$66,$60,$fc,$cf,$cc,$f0,$8c,$c9,$dd,$80,$00,$f9,$99,$f0
         db $00,$33,$b9,$01,$00,$33,$3e,$c8
-    .encodedTileDataEnd
+    .end
 
-    ds $100 - 4 - @, 0 ; filling the bootrom with 0
+ds $100 - 4 - @, 0 ; filling the bootrom with 0
 
-    ; writing to the bootrom register turning the boot off
-    .finishBoot:
-        ld a, 1
-        ld [$ff50], a
+; writing to the bootrom register turning the boot off
+FinishBoot:
+    ld a, 1
+    ld [$ff50], a
